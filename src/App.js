957e1fb2daf1ee4583cf2343ce81cfe7e921cb82
Dustan Kasten <dustan.kasten@gmail.com>
2017-11-09 09:52:37 -0500

    start setting up the scenario

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+class Checkbox extends Component {
+  state = {
+    checked: false,
+  };
+
+  toggle = () => {
+    this.setState(
+      (prevState, props) => ({ checked: !prevState.checked }),
+      () => this.props.onChange(this.state.checked)
+    );
+  };
+
+  render() {
+    return (
+      <label>
+        <input
+          type="checkbox"
+          onChange={this.toggle}
+          checked={this.state.checked}
+        />
+        {this.props.label}
+      </label>
+    );
+  }
+}
 
 class App extends Component {
+  state = {
+    shouldComponentUpdate: false,
+  };
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.shouldComponentUpdate;
+  }
+
+  toggleSCU = (shouldComponentUpdate) => {
+    this.setState({shouldComponentUpdate});
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Checkbox label="shouldComponentUpdate" onChange={this.toggleSCU} />
         </header>
-        <p className="App-intro">
-          TODO: break this!
-        </p>
       </div>
     );
   }
